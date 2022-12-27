a5eb59f7d3deb2473d11e81fc688020fbc9af1f0
jeongyong <cin03023@gmail.com>
2022-12-28 01:47:19 +0900

    Section 2-5) Test Member Domain

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -3,6 +3,8 @@ package hello.core.member;
 public class MemberServiceImpl implements MemberService {
 
     private MemberRepository memberRepository = new MemoryMemberRepository();
+    //-- DIP 위반
+    //   Interface에도 의존하고 있고, 구현Class에도 의존하고 있음
 
     @Override
     public void join(Member member) {
